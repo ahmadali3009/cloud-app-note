c5bb0950bde4020f149fdfd47ca1e65c4fb82ef0
Ahmed Ali <abutt3009@gmail.com>
2024-11-12 15:06:42 +0500

    added fixed token

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -7,6 +7,7 @@ let fetchuser = (req , res , next)=>
     token = req.header("auth-token")
     if(!token)
     {
+        token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuZXciOnsiaWQiOiI2NzMzMjgyMmVlZTFmNDY4ZjhhMTM5YWMifSwiaWF0IjoxNzMxNDA1ODU4fQ.yertkqRlZQ5dzlwIQodzwgTEAfotKxka6XNmzWAub4A"
         res.status(401).send("there is something wrong")
     }
     console.log(token)
